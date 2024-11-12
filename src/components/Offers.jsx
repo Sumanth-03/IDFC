8a8c803eebf7f1ed990e2f6b49cc893de53beb07
sumanth g r <sumanth@snowtint.com>
2024-11-12 12:38:28 +0530

    width fixes

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -206,7 +206,7 @@ function Offers (){
         <section className="flex flex-col sm:flex-row flex-wrap gap-2 justify-center items-center bg-white rounded-t-3xl md:rounded-t-none -translate-y-24 md:translate-y-0">
             {offers.map((offer)=>{
                 return(
-                    <div className="flex flex-col gap-3 md:m-2 shadow-md md:shadow-xl p-5 md:rounded-xl my-5 md:my-10 md:mx-2 w-[95%] rounded-md md:w-auto">
+                    <div className="flex flex-col gap-3 md:m-2 shadow-md md:shadow-xl p-5 md:rounded-xl my-5 md:my-10 md:mx-2 w-[95%] rounded-md md:w-[20rem]">
                     <div className="flex md:flex-col gap-3">
                     <img src={offer.icon} alt="icon" className="w-16"></img>
                     <div className="flex flex-col gap-1">

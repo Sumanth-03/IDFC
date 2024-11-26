097f556ca0da0b08c0c1b4d40f3fa7d258b4f040
sumanth g r <sumanth@snowtint.com>
2024-11-26 14:00:05 +0530

    removed some code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,8 @@ function Home (handleLogin){
     useEffect(() => {
         if(hdnRefNumber){
             setLoader(true)
-        makeApiCallWithAuth('checkPaymentStatuss',{"order_id": hdnRefNumber, "phone": sessionStorage.getItem('phone')})
+        makeApiCall('checkPaymentStatuss',{"order_id": hdnRefNumber, "phone": sessionStorage.getItem('phone')})
+        //makeApiCallWithAuth('checkPaymentStatuss',{"order_id": hdnRefNumber, "phone": sessionStorage.getItem('phone')})
         .then((response) => {
             console.log("rsd",response.data)
             setLoader(false)
@@ -463,15 +464,15 @@ function Home (handleLogin){
             }
             makeApiCall('checkOTP',indata)
             .then((response) => {
-                console.log(response?.data)
-                console.log(response?.data?.data)
+                // console.log(response?.data)
+                // console.log(response?.data?.data)
                 if(response?.data?.status === 200){
                     setOpen(false)
                     setOtpDailog(false)
                     clearInterval(time.current);  
                     setTimeLeft(10);
                     sessionStorage.setItem('otp', true)
-                    sessionStorage.setItem('token', response?.data?.data?.session)
+                    //sessionStorage.setItem('token', response?.data?.data?.session)
                     window.location.reload();
                 }
                 else if(response?.data?.status === 201){
@@ -695,7 +696,7 @@ function Home (handleLogin){
            }
          
         
-           makeApiCallWithAuth('validationCard', indata)
+           makeApiCall('validationCard', indata)
             .then((response) => {
               //console.log("resp", response?.data.data.url)
               if(response?.data?.data?.url){
